0a187d2535d4cb473a8247dc713fb04318902740
alifanbr16 <alfianbr84@gmail.com>
2024-07-12 13:22:22 +0700

    perbaiki url delete
    Signed-off-by: alifanbr16 <alfianbr84@gmail.com>

--- js/config/url.js
+++ js/config/url.js
@@ -1,4 +1,4 @@
 export let getData = 'https://backend-tb-97f8facdbf4c.herokuapp.com/makanan';
 export let insertData = 'https://backend-tb-97f8facdbf4c.herokuapp.com/ins';
-export let deleteData = 'https://backend-tb-97f8facdbf4c.herokuapp.com/ins';
+export let deleteData = 'https://backend-tb-97f8facdbf4c.herokuapp.com/delete';
 export let updateData = 'https://backend-tb-97f8facdbf4c.herokuapp.com/update';
